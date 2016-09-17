4fb5603555d4cbd37835dc3bb9dcc121cfad0796
pengxingsong <pxswork@163.com>
2016-09-17 21:35:58 +0800

    修改

--- Ngps.WebUI/Scripts/Controllers/UserController.js
+++ Ngps.WebUI/Scripts/Controllers/UserController.js
@@ -1,11 +1,11 @@
 ﻿
-ngapp.controller("UserController", function ($scope, $http) {
+ngapp.controller("UserController", ["$scope", "$http", function ($scope, $http) {
     $scope.UserList;
     $http.get("../../JData/UserList.json").success(function (data) {
         console.log(data);
         $scope.UserList = data;
     });
     $scope.orderProp = 'CreateTime';
-});
+}]);
 
 
