18aa96ca05e6e4b2f4bcc14c5d4ee4dc93210238
pengxingsong <pxswork@163.com>
2016-09-17 14:51:09 +0800

    添加项目文件。

---
+++ Ngps.WebUI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Ngps.WebUI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Ngps.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ngps.WebUI.MvcApplication" Language="C#" %>
